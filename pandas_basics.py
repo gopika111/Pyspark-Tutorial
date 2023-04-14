9b1105ac4aa875281aae2ad3084e243b79e1b4e6
gopika111 <gopikagopakumar1036@gmail.com>
2023-04-14 23:44:54 +0530

    Basics of Pandas and Sprak Installation Steps

---
+++ pandas_basics.py
@@ -0,0 +1,14 @@
+"""Spark is a computation engine.
+PySpark is a Python API used to leverage Spark features to tame big data.
+Using PySpark we can work with RDD in Python.
+
+About Resilient Distributed Dataset (RDD):
+RDD is the fundamental unit of PySpark.
+
+Working : While working on a big data, it gets converted to RDD and then for further processing of data
+the master node partitions the RDD and processes it with the help of slave nodes.
+"""
+
+
+
+
